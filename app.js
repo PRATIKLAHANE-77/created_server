231fb7b9532f8b323fe14a625c266a27b7dfd9ea
PRATIKLAHANE-77 <pratik.lahane1999@gmail.com>
2023-08-26 17:32:45 +0530

    as per user request server will giving the responce

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-// const http = require('http');
+// // Welcome to my Node Js project
 
-// const server = http.createServer((req,res)=>{
-//     res.end('hello pratik');
-// })
+// const http = require("http");
 
+// const server = http.createServer((req, res) => {
+//   res.setHeader("Content-Type", "text/html");
+//   res.write("<html>");
+//   res.write("<head><title>My First Page</title><head>");
+//   res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+//   res.write("</html>");
+//   res.end();
+// });
 
-// server.listen(4000, '127.0.0.1', () => {
-//     console.log('listning to the port number 4000');
-// })
-
+// server.listen(4000, "127.0.0.1");
 
 const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.setHeader("Content-Type", "text/html");
+    if(req.url === '/home') {
+        res.write('<html><head><title>Home</title></head><body>Welcome home</body></html>');
+    }
+    else if(req.url === '/about') {
+        res.write('<html><head><title>about</title></head><body>Welcome to About Us page</body></html>');
+    }
+    else if(req.url === '/node') {
+        res.write('<html><head><title>node</title></head><body>Welcome to my Node Js project</body></html>')
+    }
+    else{
+        res.write('<html><head><title>404 Not Found</title></head><body>Page not found</body></html>');
+        res.statusCode = 404;
+    }
+    res.end();
 
-const server = http.createServer((req, res) => {
-    res.end('Hello Pratik');
-});
+})
 
-server.listen(4000, '127.0.0.1', () => {
-    console.log('Pratik Lahane');
-});
+server.listen(4000,'127.0.0.1');
+
