9ad86f1f0a22e186bfff9458db5790874a780385
PRATIKLAHANE-77 <pratik.lahane1999@gmail.com>
2023-08-28 17:41:30 +0530

    made a first small project

--- app.js
+++ app.js
@@ -1,72 +1,51 @@
-// // Welcome to my Node Js project
-
-// const http = require("http");
-
-// const server = http.createServer((req, res) => {
-//   res.setHeader("Content-Type", "text/html");
-//   res.write("<html>");
-//   res.write("<head><title>My First Page</title><head>");
-//   res.write("<body><h1>Welcome to my Node Js project</h1></body>");
-//   res.write("</html>");
-//   res.end();
-// });
-
-// server.listen(4000, "127.0.0.1");
-
-// const http = require("http");
-// const { text } = require("stream/consumers");
-// const server = http.createServer((req, res) => {
-//   if (req.url === "/") {
-//     res.write("<html>");
-//     res.write("<head><title>enter massage</title></head>");
-//     res.write(
-//       '<body><form action = "/massage" method = "POST"><label for="file">file name:</label><input type ="text" name = "massage" id = "1"><button type = "submit">Submit</button></form></body>'
-//     );
-//     res.write("</html>");
-//     res.end();
-//   } else {
-//     res.writeHead(302);
-//     res.write("<html>");
-//     res.write("<head><title>new file</title></head>");
-//     res.write('<body><form><label for = "file"> ${document.getElementById(1).value} </label><input type = "text" name = "massage"><button type = "submit">Submit</button></form></body>');
-//     res.write("</html>");
-//     res.end();
-//   }
-// });
-
-// server.listen(3000, "127.0.0.1");
-
-const http = require("http");       // it is a server module 
+const http = require("http"); // it is a servermodule
+const fs = require('fs');
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write("<html>");
+    fs.readFile("massage.text",{encoding : "utf-8"}, (err,data)=>{
+      if(err) {
+        console.log(err);
+      }
+
+      res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
+    res.write(`<body>${data}</body>`)
     res.write(
-      '<body><form action="/" ><label for="file">File name:</label><input type="text" name="massage" id="1"><button type="submit">Submit</button></form></body>'
+      '<body><form action="/massage" method="POST"><label for="file"></label><input type="text" name="massage" id="1"><button type="submit">Submit</button></form></body>'
     );
     res.write("</html>");
-    res.end();
+    return res.end();
+  })
   } else if (req.url === "/massage" && req.method === "POST") {
-    // Handle the POST request for /massage here
-    // For simplicity, let's just respond with a message
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Submitted</title></head>");
-    // const val = document.getElementById(1).value;
-    res.write("<body><h1>name</h1></body>");
-    res.write("</html>");
-    res.end();
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    })
+    return req.on('end', () =>{
+      const parsebody = Buffer.concat(body).toString();
+      const massage = parsebody.split('=')[1];
+      fs.writeFile('massage.text', massage,err => {
+        if(err) {
+          console.log(err);
+        }
+        res.statusCode = 302;
+      res.setHeader('location', '/');
+
+       res.end();
+
+      });
+      
+    })
   } else {
     res.writeHead(404);
     res.write("<html>");
     res.write("<head><title>Not Found</title></head>");
     res.write("<body><h1>404 - Not Found</h1></body>");
     res.write("</html>");
-    res.end();
+    return res.end();
   }
 });
 
-server.listen(3000, "127.0.0.1", () => {
-  console.log("Server is listening on port 4000");
-});
+server.listen(10000, "127.0.0.1");
+
 
